f573a5e239aae4c5e6b642ee3adc4993b1ef21ac
Ednan Jhony <ednanjhony@yahoo.com.br>
2020-05-22 06:13:20 -0300

    Add routes and dashboard

--- src/components/ToastContainer/Toast/index.tsx
+++ src/components/ToastContainer/Toast/index.tsx
@@ -4,7 +4,6 @@ import {
   FiCheckCircle,
   FiInfo,
   FiXCircle,
-  FiCheck,
 } from 'react-icons/fi';
 
 import { ToastMessage, useToast } from '../../../hooks/toast';
@@ -13,6 +12,7 @@ import { Container } from './styles';
 
 interface ToastProps {
   message: ToastMessage;
+  style: object;
 }
 
 const icons = {
@@ -21,7 +21,7 @@ const icons = {
   success: <FiCheckCircle size={24} />,
 };
 
-const Toast: React.FC<ToastProps> = ({ message }) => {
+const Toast: React.FC<ToastProps> = ({ message, style }) => {
   const { removeToast } = useToast();
 
   useEffect(() => {
@@ -35,7 +35,11 @@ const Toast: React.FC<ToastProps> = ({ message }) => {
   }, [removeToast, message.id]);
 
   return (
-    <Container type={message.type} hasDescription={!!message.description}>
+    <Container
+      type={message.type}
+      hasDescription={!!message.description}
+      style={style}
+    >
       {icons[message.type || 'info']}
 
       <div>
